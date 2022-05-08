fb6454f35ad9048613ae51232c7abadcd0faacfd
temirlanmur <76197653+temirlanmur@users.noreply.github.com>
2022-05-08 15:43:52 +0600

    feat(Popup.js): add generic popup logic

--- scripts/components/Popup.js
+++ scripts/components/Popup.js
@@ -1,5 +1,40 @@
 export default class Popup {
-  constructor() {
+  constructor(popupSelector) {
+    this._popup = document.querySelector(popupSelector);
+  }
 
+  // opens popup
+  open() {
+    this._popup.classList.add('popup_opened');
+    document.addEventListener('keydown', _closePopupByEscape);
+  }
+
+  // closes popup
+  close() {
+    this._popup.classList.remove('popup_opened');
+    document.removeEventListener('keydown', _closePopupByEscape);
+  }
+
+  // sets event listeners:
+  //    on close button
+  //    on click outside modal window
+  setEventListeners() {
+    const popupCloseButton = this._popup.querySelector('.popup__close-btn');
+
+    popupCloseButton.addEventListener('click', () => {
+      this.close();
+    });
+
+    this._popup.addEventListener('click', ((evt) => {
+      if (evt.target.classList.contains('popup_opened')) {
+        this.close();
+      }
+    }));
+  }
+
+  _handleEscClose(evt) {
+    if (evt.key === "Escape") {
+      this.close();
+    }
   }
 }
