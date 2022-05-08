0d99bc8d78faed6dd29ddf03a97f1ae286fae535
temirlanmur <76197653+temirlanmur@users.noreply.github.com>
2022-05-08 18:40:01 +0600

    feat(UserInfo): add UserInfo component

---
+++ scripts/components/UserInfo.js
@@ -0,0 +1,20 @@
+export default class UserInfo {
+  constructor({ profileNameSelector, profileDescriptionSelector }) {
+    this._profileName = document.querySelector(profileNameSelector);
+    this._profileDescription = document.querySelector(profileDescriptionSelector);
+  }
+
+  // returns user data
+  getUserInfo() {
+    return {
+      profileName: this._profileName.textContent,
+      profileDescription: this._profileDescription.textContent
+    };
+  }
+
+  // sets user data
+  setUserInfo({ profileName, profileDescription }) {
+    this._profileName.textContent = profileName;
+    this.profileDescription.textContent = profileDescription;
+  }
+}
